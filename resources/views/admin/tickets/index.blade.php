3791439600f6d291970114089913f107a7bc5c68
abdelrahamn-shanan <01012617633abdoshanan@gmail.com>
2023-12-08 16:26:44 +0200

    add responsive class

--- resources/views/admin/tickets/index.blade.php
+++ resources/views/admin/tickets/index.blade.php
@@ -102,98 +102,102 @@
 
 
 	<div class="page-content">
-		<table class="table" id="ticket-table">
-			<thead>
-				<tr>
-					<th>الكود</th>
-					<th>اسم العميل</th>
-					<th>نوع الشكوى</th>
-					<th>اسم المستخدم</th>
-					<th>موجهه إلى</th>
-					<th>تاريخ الإنشاء</th>
-					<th>تاريخ اخر تحديث</th>
-					<th>الحالة</th>
-					<th>الإجراءات</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach($records as $record)
-				<tr>
-					<td>{{ $record['ticket_code'] }}</td>
-					<td>{{ $record['customer_name'] }}</td>
-					<td>
-						@if($record['type'] == 1)
-						مشكلة
-						@elseif($record['type'] == 2)
-						تحسين وتطوير
-						@elseif($record['type'] == 3)
-						متطلبات جديدة
-						@elseif($record['type'] == 4)
-						طلب خدمة
-						@else
-						Unknown Type
-						@endif
-					</td>
-					<td>{{ $record['user_name'] }}</td>
-					<td>
-						@if($record['directed_to'] == 1)
-						دعم فني
-						@elseif($record['directed_to'] == 2)
-						مبيعات
-						@elseif($record['directed_to'] == 3)
-						مالية
-						@else
-						Unknown
-						@endif
-					</td>
-					<td>{{ $record['created_at']->format('Y-m-d') }}</td>
-					<td>{{ optional($record['updated_at'])->format('Y-m-d') ?? "--" }}</td>
+		<div class="table-responsive">
 
-					<td>
-						@if($record['status'] == 1)
-						جديد
-						@elseif($record['status'] == 2)
-						جاري التحقق
-						@elseif($record['status'] == 3)
-						جاري المعالجة
-						@elseif($record['status'] == 4)
-						بإنتظار رد الجهة الطالبة
-						@elseif($record['status'] == 5)
-						بإنتظار اعتماد لجنة الحوكمة
-						@elseif($record['status'] == 6)
-						تمت المعالجة
-						@elseif($record['status'] == 7)
-						ملغاة
-						@elseif($record['status'] == 8)
-						إعادة فتح
-						@else
-						Unknown Type
-						@endif
-					</td>
-					<td>
-						<a href="{{ route('ticket.edit', $record['id']) }}">
-							<i class=" fas fa-edit"></i>
-						</a>
-						|
-						<a href="{{ route('ticket.view', $record['id']) }}">
-							<i class="fas fa-eye"></i>
-						</a>
-					</td>
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
-		<!--Pagination-->
-		<div class="custom-pagination">
-			@if($records->currentPage() > 1)
-			<a href="{{ $records->previousPageUrl() }}">Previous</a>
-			@endif
+			<table class="table" id="ticket-table">
+				<thead>
+					<tr>
+						<th>الكود</th>
+						<th>اسم العميل</th>
+						<th>نوع الشكوى</th>
+						<th>اسم المستخدم</th>
+						<th>موجهه إلى</th>
+						<th>تاريخ الإنشاء</th>
+						<th>تاريخ اخر تحديث</th>
+						<th>الحالة</th>
+						<th>الإجراءات</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach($records as $record)
+					<tr>
+						<td>{{ $record['ticket_code'] }}</td>
+						<td>{{ $record['customer_name'] }}</td>
+						<td>
+							@if($record['type'] == 1)
+							مشكلة
+							@elseif($record['type'] == 2)
+							تحسين وتطوير
+							@elseif($record['type'] == 3)
+							متطلبات جديدة
+							@elseif($record['type'] == 4)
+							طلب خدمة
+							@else
+							Unknown Type
+							@endif
+						</td>
+						<td>{{ $record['user_name'] }}</td>
+						<td>
+							@if($record['directed_to'] == 1)
+							دعم فني
+							@elseif($record['directed_to'] == 2)
+							مبيعات
+							@elseif($record['directed_to'] == 3)
+							مالية
+							@else
+							Unknown
+							@endif
+						</td>
+						<td>{{ $record['created_at']->format('Y-m-d') }}</td>
+						<td>{{ optional($record['updated_at'])->format('Y-m-d') ?? "--" }}</td>
 
-			@if($records->hasMorePages())
-			<a href="{{ $records->nextPageUrl() }}">Next</a>
-			@endif
+						<td>
+							@if($record['status'] == 1)
+							جديد
+							@elseif($record['status'] == 2)
+							جاري التحقق
+							@elseif($record['status'] == 3)
+							جاري المعالجة
+							@elseif($record['status'] == 4)
+							بإنتظار رد الجهة الطالبة
+							@elseif($record['status'] == 5)
+							بإنتظار اعتماد لجنة الحوكمة
+							@elseif($record['status'] == 6)
+							تمت المعالجة
+							@elseif($record['status'] == 7)
+							ملغاة
+							@elseif($record['status'] == 8)
+							إعادة فتح
+							@else
+							Unknown Type
+							@endif
+						</td>
+						<td>
+							<a href="{{ route('ticket.edit', $record['id']) }}">
+								<i class=" fas fa-edit"></i>
+							</a>
+							|
+							<a href="{{ route('ticket.view', $record['id']) }}">
+								<i class="fas fa-eye"></i>
+							</a>
+						</td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+			<!--Pagination-->
+			<div class="custom-pagination">
+				@if($records->currentPage() > 1)
+				<a href="{{ $records->previousPageUrl() }}">Previous</a>
+				@endif
+
+				@if($records->hasMorePages())
+				<a href="{{ $records->nextPageUrl() }}">Next</a>
+				@endif
+			</div>
 		</div>
 	</div>
+
 </div>
 @stop
 @section('script')
